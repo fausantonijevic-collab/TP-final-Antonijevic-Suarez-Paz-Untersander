51df989ade3ab1adc866e69d3d50ca39d18a3d32
Clara Untersander <cuntersanderudesa.edu.ar>
2026-06-03 15:34:24 -0300

    info de mapa, ventana y FPS, velocidades(pacman)

---
+++ setting.py
@@ -0,0 +1,16 @@
+#tamaño de tiles y mapa
+tile_size= 24
+map_col = 28
+map_filas= 31
+
+#ventana y FPS
+ancho = tile_size * map_col
+alto = tile_size* map_filas + 60
+
+fps = 60
+
+#velocidades
+vel_max = 7.5 *tile_size
+vel_pacman_normal = vel_max * 0.80
+vel_pacman_power = vel_max * 0.90
+
