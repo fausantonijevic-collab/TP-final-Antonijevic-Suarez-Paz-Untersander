2563971aa6f687eb70e651e787e87549d0b398cf
Clara Untersander <cuntersanderudesa.edu.ar>
2026-06-03 15:50:50 -0300

    info powerP(fantasmas) , puntos y vidas, colores ( fondo=negro, pared=azul, puntos=blancos, pacman=amarillo y puntajes/vidas=blanco)

--- setting.py
+++ setting.py
@@ -14,3 +14,18 @@ vel_max = 7.5 *tile_size
 vel_pacman_normal = vel_max * 0.80
 vel_pacman_power = vel_max * 0.90
 
+#power pellet
+duracion_asustado = 6.0
+duracion_parpadeo= 2.0
+
+#puntos y vidas
+vidas_iniciales= 3
+punto_dot=10
+puntos_power_pellet= 50
+
+#Colores
+color_fondo= (0,0,0)
+color_pared =(28,42,237)
+color_dot = (255,255,255)
+color_pacman=  (255,255,0)
+color_infojuego = (255,255,255)
