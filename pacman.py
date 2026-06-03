9e40a62031c9767c1799ae16a0acf8e07dc630ba
Clara Untersander <cuntersanderudesa.edu.ar>
2026-06-03 18:58:33 -0300

    defini: direccion/mov , boca del pacman, caracteres transitar pacman

--- pacman.py
+++ pacman.py
@@ -1,3 +1,9 @@
 import pygame
 import math
-from setting import ()
+from setting import (tile_size, vel_pacman_normal, vel_pacman_power,color_pacman,duracion_asustado)
+
+direcciones= {"derecha":(1,0) , "izquierda":(-1,0), "arriba":(0,-1),"abajo":(0,1)}
+
+angulo_base={"derecha":0 , "izquierda":180, "arriba":90,"abajo":270}
+
+tiles_transitables = {"",".","o","P","T","-"}
