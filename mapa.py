4e48e499cd6bc861768b3fa2cf16c9a278530d85
sofisuarezz <sofisuarez2325@gmail.com>
2026-06-10 08:32:11 -0300

    correcciones de mapa

--- mapa.py
+++ mapa.py
@@ -1,9 +1,8 @@
+from setting import tile_size, map_col, map_filas
 caracteres_validos = {"X", "o", ".", " ", "G", "T", "-", "P"}
 
-ancho_mapa = 28
-alto_mapa = 31
-
-from setting import tile_size
+ancho_mapa = map_col
+alto_mapa = map_filas
 
 class Mapa:
     def __init__(self, ruta):
