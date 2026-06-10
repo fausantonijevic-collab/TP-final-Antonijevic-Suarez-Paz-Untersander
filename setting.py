193406d04698c4663042903ec6c886ff59d7e9e3
sofisuarezz <sofisuarez2325@gmail.com>
2026-06-10 08:32:26 -0300

    agrego mas velocidades de los ghosts

--- setting.py
+++ setting.py
@@ -13,6 +13,10 @@ fps = 60
 vel_max = 7.5 *tile_size
 vel_pacman_normal = vel_max * 0.80
 vel_pacman_power = vel_max * 0.90
+vel_fantasma_normal = vel_max * 0.75
+vel_fantasma_asustado = vel_max * 0.50
+vel_fantasma_tunel = vel_max * 0.40
+vel_fantasma_ojos = vel_max * 1.50
 
 #power pellet
 duracion_asustado = 6.0
